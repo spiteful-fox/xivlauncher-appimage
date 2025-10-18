64b5189be88241fc4382f54da847b3c91f5d07c1
Zorua <goodly_mimic_0l@icloud.com>
2025-10-18 17:39:02 -0400

    xivlauncher-rb: fix using umu with nvidia drivers on debian/fedora based distros

--- appimage-build/resources/xivlauncher.sh
+++ appimage-build/resources/xivlauncher.sh
@@ -2,7 +2,9 @@
 
 export APPIMAGE_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export LD_LIBRARY_PATH=$APPIMAGE_ROOT/usr/lib:$LD_LIBRARY_PATH
-export PATH=$APPIMAGE_ROOT/usr/bin:$APPIMAGE_ROOT/usr/lib:$PATH
+OLD_LIBRARY_SEARCH_PATH=$(ldconfig -v 2>/dev/null | grep -E '^/.*:' | cut -d: -f1 | tr '\n' ':' | sed 's/:$//')
+export LD_LIBRARY_PATH=$APPIMAGE_ROOT/usr/lib:$OLD_LIBRARY_SEARCH_PATH:$LD_LIBRARY_PATH
+
+export PATH=$APPIMAGE_ROOT/usr/bin:$PATH
 
 $APPIMAGE_ROOT/opt/%{executable}/XIVLauncher.Core $@
