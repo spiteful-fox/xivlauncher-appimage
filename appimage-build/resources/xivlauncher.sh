3d526dd6ad449cb040b9e316f7f5c7efbf9bbef1
Zorua <goodly_mimic_0l@icloud.com>
2025-10-26 21:10:35 -0400

    Use $APPDIR instead of making stuff up

--- appimage-build/resources/xivlauncher.sh
+++ appimage-build/resources/xivlauncher.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-export APPIMAGE_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 OLD_LIBRARY_SEARCH_PATH=$(ldconfig -v 2>/dev/null | grep -E '^/.*:' | cut -d: -f1 | tr '\n' ':' | sed 's/:$//')
-export LD_LIBRARY_PATH=$APPIMAGE_ROOT/usr/lib:$OLD_LIBRARY_SEARCH_PATH:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$APPDIR/usr/lib:$OLD_LIBRARY_SEARCH_PATH:$LD_LIBRARY_PATH
 
-export PATH=$APPIMAGE_ROOT/usr/bin:$PATH
+export PATH=$APPDIR/usr/bin:$PATH
 
-$APPIMAGE_ROOT/opt/%{executable}/XIVLauncher.Core $@
+$APPDIR/opt/%{executable}/XIVLauncher.Core $@
