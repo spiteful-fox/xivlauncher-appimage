5b2522d046e69ba1557de4b02f646c219b99e128
Zorua <goodly_mimic_0l@icloud.com>
2026-01-28 13:44:57 -0500

    Default to X11 on GNOME Wayland (so that window decorations work)

--- appimage-build/resources/xivlauncher.sh
+++ appimage-build/resources/xivlauncher.sh
@@ -5,4 +5,8 @@ export LD_LIBRARY_PATH=$APPDIR/usr/lib:$OLD_LIBRARY_SEARCH_PATH:$LD_LIBRARY_PATH
 
 export PATH=$APPDIR/usr/bin:$PATH
 
+if [ ! -z "$WAYLAND_DISPLAY" ] && ( [ "$XDG_CURRENT_DESKTOP" == "GNOME" ] || [ "$XDG_CURRENT_DESKTOP" == "ubuntu:GNOME" ] ); then
+    export SDL_VIDEO_DRIVER="${SDL_VIDEO_DRIVER:-x11}"
+fi
+
 $APPDIR/opt/%{executable}/XIVLauncher.Core $@
